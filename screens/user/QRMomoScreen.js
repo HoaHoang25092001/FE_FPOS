80b5c70c285bde490f1f0a9c34d4e12c09744c9e
HoaHoangDev <hquanghoa25092001@gmail.com>
2023-07-19 16:49:23 +0700

    cua hoa

---
+++ screens/user/QRMomoScreen.js
@@ -0,0 +1,12 @@
+import React  from 'react'
+import { Text, View } from 'react-native'
+
+const QRMomoScreen = () => {
+    return (
+      <View>
+        <Text> QRMomoScreen </Text>
+      </View>
+    )
+}
+
+export default QRMomoScreen
